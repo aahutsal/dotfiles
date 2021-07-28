3db37166449d66f3db18cc76cc4c234fb53d9772
Arsen A. Hutsal <gutsal.arsen@gmail.com>
2021-07-28 14:11:58 +0300

    company-tabnine added, tab-line preserved

--- .my/init.el
+++ .my/init.el
@@ -12,3 +12,12 @@
   :ensure t
   :config (beacon-mode 1))
 
+(use-package company-tabnine
+  :ensure t
+  :config (add-to-list 'company-backends #'company-tabnine))
+
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
